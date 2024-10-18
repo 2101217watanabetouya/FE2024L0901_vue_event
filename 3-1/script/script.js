386753bee4ab75ed9f75432450fadf17c4236f75
2101217watanabetouya <2101217@s.asojuku.ac.jp>
2024-10-18 09:51:21 +0900

    イベントハンドリング機能の追加

--- 3-1/script/script.js
+++ 3-1/script/script.js
@@ -3,7 +3,12 @@ new Vue({
     el: '#app',
     data() {
         return {
-            
+            text: 'hello Vue.js'
         };
+    },
+    method:{
+        handleClick() {
+            this.text = 'button pushed';
+        }
     }
 });
